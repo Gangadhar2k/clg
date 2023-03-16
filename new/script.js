9ac02b785a483e358b22ae178403c01fc1f0544e
Gangadhar S <115092868+Gangadhar2k@users.noreply.github.com>
2023-03-16 12:20:14 +0530

    first
    completed

---
+++ new/script.js
@@ -0,0 +1,41 @@
+var cwndndt = new Date("mar 31, 2023 10:00:00").getTime();
+var x = setInterval(function () {
+  var now = new Date().getTime();
+  var dist = cwndndt - now;
+  var d = Math.floor(dist / (1000 * 60 * 60 * 24));
+  var h = Math.floor((dist % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var m = Math.floor((dist % (1000 * 60 * 60)) / (1000 * 60));
+  var s = Math.floor((dist % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = d;
+  document.getElementById("hours").innerHTML = h;
+  document.getElementById("minutes").innerHTML = m;
+  document.getElementById("seconds").innerHTML = s;
+}, 1000);
+
+function btq() {
+  location.href = "https://forms.gle/GaM15UMw8r4rZbfC6";
+}
+
+function cddb() {
+  location.href = "https://forms.gle/GaM15UMw8r4rZbfC6";
+}
+function tq() {
+  location.href = "https://forms.gle/GaM15UMw8r4rZbfC6";
+}
+function prsw() {
+  location.href = "https://forms.gle/GaM15UMw8r4rZbfC6";
+}
+function fndq() {
+  location.href = "https://forms.gle/GaM15UMw8r4rZbfC6";
+}
+function bxc() {
+  location.href = "https://forms.gle/GaM15UMw8r4rZbfC6";
+}
+function shpr() {
+  location.href = "https://forms.gle/GaM15UMw8r4rZbfC6";
+}
+function cnxs() {
+  location.href = "https://forms.gle/GaM15UMw8r4rZbfC6";
+}
+
